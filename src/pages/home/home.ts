0b8a3b1465414588c0235e56cee572d79a17dc87
Jakub Flaska <jakub.flaska@gmail.com>
2018-03-21 11:51:03 -0400

    x

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+// import { HTTP } from '@ionic-native/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,18 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  posts;
 
+  constructor(public navCtrl: NavController, private http: HttpClient) {
+    this.getData();
+  }
+
+  getData(){
+
+    this.http.get('https://www.reddit.com/r/gifs/new/.json?limit=10').subscribe(data => {
+        this.posts = data.data.children;
+        console.log(this.posts);
+    });
   }
 
 }
